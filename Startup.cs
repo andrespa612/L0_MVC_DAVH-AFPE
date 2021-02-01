72e07664f698b8ca87455309acee5512fd5d5a3b
ANDRES FERNANDO PARADA ESTRADA <afparada@correo.url.edu.gt>
2021-02-01 13:59:15 -0600

    Start changed

--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace L0_MVC_DAVH_AFPE
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Clients}/{action=Index}/{id?}");
             });
         }
     }
