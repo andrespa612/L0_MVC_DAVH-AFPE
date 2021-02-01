8497233e60655315797bf704d9585cde102f11cd
ANDRES FERNANDO PARADA ESTRADA <afparada@correo.url.edu.gt>
2021-02-01 13:33:43 -0600

    Client Model

---
+++ Models/ClientsModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace L0_MVC_DAVH_AFPE.Models
+{
+    public class ClientsModel
+    {
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Description { get; set; }
+    }
+}
