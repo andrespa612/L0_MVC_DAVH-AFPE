06ae8e40cef33b0cbe2a5bb6b213ea7f0f1938a6
ANDRES FERNANDO PARADA ESTRADA <afparada@correo.url.edu.gt>
2021-02-01 12:44:57 -0600

    Agregar archivos de proyecto.

---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace L0_MVC_DAVH_AFPE.Models
+{
+    public class Student
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string name { get; set; }
+        
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Birth { get; set; }
+        public int age { get; set; }
+    }
+}
