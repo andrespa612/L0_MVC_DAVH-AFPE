366f6c2e1bcb703aadb306baf4f1728ccbc3f873
danielvh01 <danielvelasquez32@gmail.com>
2021-02-03 16:22:17 -0600

    MASTER

--- Models/ClientsModel.cs
+++ Models/ClientsModel.cs
@@ -1,17 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace L0_MVC_DAVH_AFPE.Models
 {
     public class ClientsModel
     {   
+        
         public string Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         public string Lastname { get; set; }
-
+        [Required]
         public string PhoneNumber { get; set; }
-
+        [Required]
         public string Description { get; set; }
     }
 }
